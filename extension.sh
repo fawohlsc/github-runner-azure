cb6794fc430b02e4b937225bc949c9347d04acca
fawohlsc <fawohlsc@microsoft.com>
2020-06-09 23:34:13 +0200

    Added GHA runner and config to extension.sh

--- extension.sh
+++ extension.sh
@@ -7,5 +7,5 @@ mkdir actions-runner && cd actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
 
-./config.sh --url https://github.com/fawohlsc/gha-self-hosted-runner --token $GITHUB_TOKEN
-./run.sh
+#./config.sh --url https://github.com/fawohlsc/gha-self-hosted-runner --token $GITHUB_TOKEN
+#./run.sh
