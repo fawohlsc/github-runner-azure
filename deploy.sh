e6377491fbefec117f0177b14cbba1c2737fe61e
fawohlsc <fawohlsc@microsoft.com>
2020-06-09 23:30:42 +0200

    Added GHA runner and config to extension.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ PROJECT_NAME="gha-self-hosted-runner"
 RESOURCE_GROUP_NAME="$PROJECT_NAME-rg"
 VM_NAME="$PROJECT_NAME-vm"
 VM_EXTENSION_NAME="$VM_NAME-ext"
-PUBLISHER="Microsoft.Azure.Extensions"
+PUBLISHER="fawohlsc"
 IMAGE="UbuntuLTS"
 LOCATION="WestEurope"
 ADMIN_USERNAME=$PROJECT_NAME
@@ -22,6 +22,7 @@ az vm create \
   --admin-username $ADMIN_USERNAME \
   --generate-ssh-keys
 
+# VM Extension
 az vm extension set \
   --resource-group $RESOURCE_GROUP_NAME \
   --vm-name $VM_NAME \
@@ -29,12 +30,3 @@ az vm extension set \
   --publisher Microsoft.Azure.Extensions \
   --force-update \
   --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/fawohlsc/gha-self-hosted-runner/master/extension.sh"],"commandToExecute": "./extension.sh"}'
-
-# VM Extension
-#az vm extension set \
-#--resource-group $RESOURCE_GROUP_NAME \
-#--vm-name $VM_NAME \
-#--name $VM_EXTENSION_NAME \
-#--publisher $PUBLISHER \
-#--force-update \
-#--protected-settings '{"fileUris": ["https://raw.githubusercontent.com/fawohlsc/gha-self-hosted-runner/master/extension.sh"],"commandToExecute": "./extension.sh"}'
--- extension.sh
+++ extension.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-touch /home/gha-self-hosted-runner/foo
+#https://jessicadeen.com/github-actions-self-hosted-runner/
+
+# Download GitHub Actions self-hosted runner
+mkdir actions-runner && cd actions-runner
+curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
+
+./config.sh --url https://github.com/fawohlsc/gha-self-hosted-runner --token $GITHUB_TOKEN
+./run.sh
