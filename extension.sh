73e53964029646921ed08074b8a0aaa891147623
fawohlsc <fawohlsc@microsoft.com>
2020-06-11 14:26:39 +0200

    Fixed extension.sh

--- extension.sh
+++ extension.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#https://jessicadeen.com/github-actions-self-hosted-runner/
-
-# Download GitHub Actions self-hosted runner
-pwd > ~/pwd.txt
+echo "Downloading GitHub Actions self hosted runner to [$(pwd)]..."
 mkdir actions-runner && cd actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
 
-#./config.sh --url https://github.com/fawohlsc/gha-self-hosted-runner --token $GITHUB_TOKEN
-#./run.sh
+echo "Extracting GitHub Actions self hosted runner to [$(pwd)]..."
+tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
+rm ./actions-runner-linux-x64-2.263.0.tar.gz
+
+echo "Configuring GitHub Actions self hosted runner..."
+./config.sh --unattended --name $GITHUB_RUNNER_NAME --labels $GITHUB_RUNNER_LABEL --url https://github.com/fawohlsc/gha-self-hosted-runner --token $GITHUB_TOKEN
+
+echo "Installing GitHub Actions self hosted runner as a service..."
+sudo ./svc.sh install
+sudo ./svc.sh start
+sudo ./svc.sh status
