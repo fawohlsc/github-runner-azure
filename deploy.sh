005226807cdc0c13e062c031e0f0a8ea309747ed
fawohlsc <fawohlsc@microsoft.com>
2020-06-11 12:25:52 +0200

    Fixed extension.sh

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,63 @@
   
 #!/bin/bash
+# Suggested improvements:
+# - Proper error handling 
+# - Refactoring into bash functions 
+# Good example: https://github.com/paolosalvatori/front-door-apim/blob/master/scripts/deploy.sh
 
 # Variables
-PROJECT_NAME="gha-self-hosted-runner"
-RESOURCE_GROUP_NAME="$PROJECT_NAME-rg"
-VM_NAME="$PROJECT_NAME-vm"
-VM_EXTENSION_NAME="$VM_NAME-ext"
-PUBLISHER="fawohlsc"
-IMAGE="UbuntuLTS"
-LOCATION="WestEurope"
-ADMIN_USERNAME=$PROJECT_NAME
+projectName="gha-self-hosted-runner"
+rgName="$projectName-rg"
+vmName="$projectName-vm"
+vmExtensionName="customScript"
+vmExtensionFileUri="https://raw.githubusercontent.com/fawohlsc/gha-self-hosted-runner/master/extension.sh"
+vmExtensionCommandToExecute="./extension.sh"
+publisher="Microsoft.Azure.Extensions"
+image="UbuntuLTS"
+location="WestEurope"
+adminUserName=$projectName
+
+# SubscriptionId of the current subscription
+subscriptionId=$(az account show --query id --output tsv)
 
 # Resource Group
-az group create -l $LOCATION -n $RESOURCE_GROUP_NAME
+echo "Checking if resource group [$rgName] exits in subscription [$subscriptionId]..."
+if az group show --name $rgName 1>/dev/null; then
+  echo "Found resource group [$rgName] in subscription [$subscriptionId]."
+else 
+  echo "Creating resource group [$rgName] in subscription [$subscriptionId]..."
+  az group create --location $location --name $rgName --query id --output tsv
+fi
+printf "\n"
 
 # VM
-az vm create \
-  --resource-group $RESOURCE_GROUP_NAME \
-  --name $VM_NAME \
-  --image $IMAGE \
-  --admin-username $ADMIN_USERNAME \
-  --generate-ssh-keys
+echo "Checking if VM [$vmName] exists in resource group [$rgName]..."
+if az vm show --resource-group $rgName --name $vmName 1>/dev/null; then
+  echo "Found VM [$vmName] in resource group [$rgName]."
+else 
+  echo "Creating VM [$vmName] in resource group [$rgName]..."
+  az vm create \
+    --resource-group $rgName \
+    --name $vmName \
+    --image $image \
+    --admin-username $adminUserName \
+    --generate-ssh-keys
+fi
+printf "\n"
 
 # VM Extension
+echo "Checking if VM extension [$vmExtensionName] exists in VM [$vmName]..."
+if az vm extension show --resource-group $rgName --vm-name $vmName --name $vmExtensionName 2>/dev/null; then
+  echo "Found VM extension [$vmExtensionName] in VM [$vmName]."
+
+  echo "Deleting VM extension [$vmExtensionName] in VM [$vmName]..."
+  az vm extension delete --resource-group $rgName --vm-name $vmName --name $vmExtensionName
+fi
+
+echo "Installing VM extension [$vmExtensionName] in VM [$vmName]..."
 az vm extension set \
-  --resource-group $RESOURCE_GROUP_NAME \
-  --vm-name $VM_NAME \
-  --name customScript \
-  --publisher Microsoft.Azure.Extensions \
-  --force-update \
-  --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/fawohlsc/gha-self-hosted-runner/master/extension.sh"],"commandToExecute": "./extension.sh"}'
+  --resource-group $rgName \
+  --vm-name $vmName \
+  --name $vmExtensionName \
+  --publisher $publisher \
+  --protected-settings "{'fileUris': ['$vmExtensionFileUri'],'commandToExecute': '$vmExtensionCommandToExecute'}"
--- extension.sh
+++ extension.sh
@@ -3,6 +3,7 @@
 #https://jessicadeen.com/github-actions-self-hosted-runner/
 
 # Download GitHub Actions self-hosted runner
+pwd > pwd.txt
 mkdir actions-runner && cd actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
