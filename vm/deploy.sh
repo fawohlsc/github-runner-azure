699621ad39bd770f70339e17254e51a5c75487b9
fawohlsc <fawohlsc@microsoft.com>
2020-06-16 17:15:54 +0200

    Created vm deploy

--- vm/deploy.sh
+++ vm/deploy.sh
@@ -6,46 +6,25 @@ set -e -u # Exit script on error and treat unset variables as an error
 BLUE="\033[0;34m"
 GREEN="\033[0;32m"
 NC="\033[0m" # No Color
-RANDOM_STRING=$(head /dev/urandom | tr -dc a-z0-9 | head -c 13)
-ACR_NAME=${RANDOM_STRING}
-ACR_SKU="Basic"
-ACR_URL="${ACR_NAME}.azurecr.io"
 VM_NAME="${RG_NAME}VM"
 VM_IMAGE="UbuntuLTS"
 VM_ADMIN=${RG_NAME}
 VM_EXT_NAME="customScript"
 VM_EXT_PUBLISHER="Microsoft.Azure.Extensions"
-VM_EXT_FILE_URIS="'https://raw.githubusercontent.com/${GH_REPOSITORY}/master/docker/install-docker.sh','https://raw.githubusercontent.com/${GH_REPOSITORY}/master/docker/install-github-runner.sh'"
+VM_EXT_FILE_URIS="'https://raw.githubusercontent.com/${GH_REPOSITORY}/master/vm/get-token.sh','https://raw.githubusercontent.com/${GH_REPOSITORY}/master/vm/install-github-runner.sh'"
 NSG_NAME="${RG_NAME}VMNSG"
 NSG_RULE_NAME="default-allow-ssh"
-RUNNER_IMAGE_SOURCE="docker.io/myoung34/github-runner:latest"
-RUNNER_IMAGE="github-runner"
-RUNNER_IMAGE_TAG="latest" # TODO: Proper versioning of container image
 RUNNER_NAME=${RG_NAME}
 RUNNER_USER=${VM_ADMIN} # TODO: Do not run GitHub Runner under VM Admin
 RUNNER_LABELS="Azure"
 REPO_URL="https://github.com/${GH_REPOSITORY}"
 # TODO: Do not pass GH_TOKEN via bash
-VM_EXT_COMMAND="./docker/install-docker.sh && ./docker/install-github-runner.sh ${ACR_NAME} ${RUNNER_IMAGE} ${RUNNER_IMAGE_TAG} ${RUNNER_NAME} ${RUNNER_USER} ${GH_TOKEN} ${REPO_URL} ${RUNNER_LABELS}"
+VM_EXT_COMMAND="./install-github-runner.sh ${ACR_NAME} ${RUNNER_IMAGE} ${RUNNER_IMAGE_TAG} ${RUNNER_NAME} ${RUNNER_USER} ${GH_TOKEN} ${REPO_URL} ${RUNNER_LABELS}"
 
 echo -e "${BLUE}Executing deployment...${NC}"
 
 echo -e "${GREEN}Creating resource group [${RG_NAME}] in location [${RG_LOCATION}]...${NC}"
 az group create --location ${RG_LOCATION} --name ${RG_NAME}
-
-echo -e "${GREEN}Creating container registry [${ACR_NAME}] in resource group [${RG_NAME}]...${NC}"
-az acr create --resource-group ${RG_NAME} --name ${ACR_NAME} --sku ${ACR_SKU}
-ACR_ID=$(az acr show  \
-  --resource-group ${RG_NAME} \
-  --name ${ACR_NAME} \
-  --query id \
-  --output tsv)
-
-echo -e "${GREEN}Importing runner image [${RUNNER_IMAGE}] into container registry [${ACR_NAME}]...${NC}"
-az acr import \
-  --name ${ACR_NAME} \
-  --source "${RUNNER_IMAGE_SOURCE}" \
-  --image "${RUNNER_IMAGE}:${RUNNER_IMAGE_TAG}"
 
 echo -e "${GREEN}Creating VM [${VM_NAME}] in resource group [${RG_NAME}]...${NC}"
 az vm create \
@@ -55,27 +34,6 @@ az vm create \
   --admin-username $VM_ADMIN \
   --generate-ssh-keys \
   --public-ip-address "" # Only private IP address
-
-echo -e "${GREEN}Deleting NSG rule [${NSG_RULE_NAME}] in NSG [${NSG_NAME}]...${NC}"
-az network nsg rule delete -g ${RG_NAME} --nsg-name ${NSG_NAME} -n ${NSG_RULE_NAME}
-
-echo -e "${GREEN}Configuring VM [${VM_NAME}] with system-managed identity...${NC}"
-az vm identity assign \
- --resource-group ${RG_NAME} \
- --name ${VM_NAME} 
-VM_IDENTITY=$(az vm show  \
-  --resource-group ${RG_NAME}  \
-  --name ${VM_NAME} \
-  --query identity.principalId \
-  --out tsv)
-
-echo -e "${GREEN}Granting system-managed identity [${VM_IDENTITY}] access to container registry [${ACR_ID}]...${NC}"
-# Use assignee-object-id instead of assignee to avoid errors caused by propagation latency in AAD Graph
-az role assignment create   \
-  --assignee-object-id ${VM_IDENTITY}   \
-  --assignee-principal-type ServicePrincipal \
-  --scope ${ACR_ID}   \
-  --role acrpull
 
 echo -e "${GREEN}Installing VM extension [${VM_EXT_NAME}] in VM [${VM_NAME}]...${NC}"
 az vm extension set \
---
+++ vm/install-github-runner.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e -u # Exit script on error and treat unset variables as an error
+
+RUNNER_URL="https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz"
+RUNNER_PACKAGE="./actions-runner-linux-x64-2.263.0.tar.gz"
+RUNNER_NAME="github-runner-vm"
+GITHUB_TOKEN="AJ3UTYFBPRYPDDFTKGRULYC65DV72"
+REPO_URL="https://github.com/fawohlsc/github-runner-azure "
+LABELS="Azure,VM"
+
+export RUNNER_ALLOW_RUNASROOT=1 # TODO: Do not run as root
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+NC="\033[0m" # No Color
+
+echo -e "${BLUE}Installing GitHub Runner...${NC}"
+
+echo -e "${GREEN}Downloading GitHub Runner package...${NC}"
+mkdir actions-runner && cd actions-runner
+curl -O -L ${RUNNER_URL}
+
+echo -e "${GREEN}Extracting GitHub Runner package...${NC}"
+tar xzf ${RUNNER_PACKAGE}
+
+echo -e "${GREEN}Configure the GitHub Runner ...${NC}"
+chmod +x ./config.sh
+./config.sh \
+  --url ${REPO_URL} \
+  --token ${GITHUB_TOKEN} \
+  --name ${RUNNER_NAME} \
+  --labels "${LABELS}" \
+  --unattended \
+  --replace
+
+#TODO: Start GitHub runner as service to cover VM reboots
+echo -e "${GREEN}Run the GitHub Runner ...${NC}"
+chmod +x ./run.sh
+./run.sh
+
+echo -e "${BLUE}GitHub Runner installed successfully.${NC}"
