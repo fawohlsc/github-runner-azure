f94eb4f9b8235c69a5bd6e49da51d1145404aa33
fawohlsc <fawohlsc@microsoft.com>
2020-06-16 15:07:06 +0200

    Foo

--- foo.sh
+++ foo.sh
@@ -3,9 +3,30 @@
 
 set -e -u # Exit script on error and treat unset variables as an error
 
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+NC="\033[0m" # No Color
+ACR_NAME="${RG_NAME}ACR"
+RUNNER_IMAGE_SOURCE="docker.io/myoung34/github-runner:latest"
+RUNNER_IMAGE="github-runner"
+RUNNER_IMAGE_TAG=${GITHUB_RUN_NUMBER}
+VM_NAME="${RG_NAME}VM"
+VM_IMAGE="UbuntuLTS"
+VM_ADMIN=${RG_NAME}
+VM_EXT_NAME="customScript"
+VM_EXT_PUBLISHER="Microsoft.Azure.Extensions"
+VM_EXT_FILE_URIS="'https://raw.githubusercontent.com/${GITHUB_REPOSITORY}/master/install-docker.sh','https://raw.githubusercontent.com/${{github.GITHUB_REPOSITORY}}/master/install-github-runner.sh'"
+NSG_NAME="${RG_NAME}NSG"
+NSG_RULE_NAME="default-allow-ssh"
+RUNNER_NAME=${RG_NAME}
+RUNNER_USER=${VM_ADMIN} # Do not run GitHub Runner under VM Admin
+RUNNER_LABELS="Azure"
+REPO_URL="https://github.com/${GITHUB_REPOSITORY}"
+VM_EXT_COMMAND="./install-docker.sh && ./install-github-runner.sh ${ACR_NAME} ${RUNNER_IMAGE} ${RUNNER_IMAGE_TAG} ${RUNNER_NAME} ${RUNNER_USER} ${GH_TOKEN_REPO} ${REPO_URL} ${RUNNER_LABELS}"
+
 echo -e "${BLUE}Executing deployment...${NC}"
 
-echo -e "${GREEN}Creating resource group [${RG_NAME}] in subscription [${SUBSCRIPTION_ID}]...${NC}"
+echo -e "${GREEN}Creating resource group [${RG_NAME}] in location [${RG_LOCATION}]...${NC}"
 
 echo -e "${GREEN}Creating container registry [${ACR_NAME}] in resource group [${RG_NAME}]...${NC}"
 
